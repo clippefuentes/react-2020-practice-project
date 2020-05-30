1413e1b80653efe81a8ff6a7b523fad36c8c09f8
Clyne Paulo Fuentes <clippefuentes@gmail.com>
2020-05-29 18:16:25 -0600

    Add navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+
+const TopicList = (props) => {
+  return(
+  <div>
+    <h1>Topic List Page </h1>
+  </div>
+)};
+
+const TopicDetail = (props) => (
+  <div>
+    <h1>Topic Detail Page: {props.match.params.topicId} </h1>
+  </div>
+);
+
+
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/topics" component={TopicList} />
+        <Route path="/topics/:topicId" component={TopicDetail} />
+      </Switch>
     </div>
   );
 }
