40f40d5b4397624910f218190fe941d31cd47f46
Clyne Paulo Fuentes <clippefuentes@gmail.com>
2020-05-29 21:35:48 -0600

    Add new components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
+
 import Homepage from './pages/homepage/homepage.component';
-
-const TopicList = (props) => {
-  return(
-  <div>
-    <h1>Topic List Page </h1>
-  </div>
-)};
-
-const TopicDetail = (props) => (
-  <div>
-    <h1>Topic Detail Page: {props.match.params.topicId} </h1>
-  </div>
-);
-
-
+import Shoppage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route exact path="/topics" component={TopicList} />
-        <Route path="/topics/:topicId" component={TopicDetail} />
+        <Route exact path="/shop" component={Shoppage} />
       </Switch>
     </div>
   );
