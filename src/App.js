338d5325483ab73f08429dc84c773f7c3919f0f2
Clyne Paulo Fuentes <clippefuentes@gmail.com>
2020-05-30 23:02:42 -0600

    Add Sign In page and components

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import './App.css';
 import Header from './components/header/header.component';
 import Homepage from './pages/homepage/homepage.component';
 import Shoppage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
     <div>
       <Header />
       <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route exact path="/shop" component={Shoppage} />
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop' component={Shoppage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
