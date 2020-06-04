d7fb887d1b7b63a87bc28dce8a39a9a9f8e27ec2
Clyne Paulo Fuentes <clippefuentes@gmail.com>
2020-06-03 20:28:37 -0600

    Implement firebase sign up

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from './components/header/header.component';
 import Homepage from './pages/homepage/homepage.component';
 import Shoppage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 class App extends React.Component {
   constructor() {
@@ -20,9 +20,24 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapshop => {
+          this.setState({
+            currentUser: {
+              id: snapshop.id,
+              ...snapshop.data(),
+            }
+          }, () => {
+            console.log(this.state)
+          });
+        });
+      }
+      this.setState( {
+        currentUser: userAuth,
+      })
     });
   }
 
