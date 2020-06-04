f64976cf103efabb1368a65b5fde84f2eeaccf6f
Clyne Paulo Fuentes <clippefuentes@gmail.com>
2020-06-03 20:33:16 -0600

    Add Sign In handle

--- src/App.js
+++ src/App.js
@@ -31,7 +31,6 @@ class App extends React.Component {
               ...snapshop.data(),
             }
           }, () => {
-            console.log(this.state)
           });
         });
       }
