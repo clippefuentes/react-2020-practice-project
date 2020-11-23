e97219b3440a36b81004a2d22beaeb904fac95ab
Clyne Paulo Fuentes <clippefuentes@gmail.com>
2020-11-23 14:58:29 -0700

    changes on functions

--- functions/src/FUNCTION_1/FUNC/runFunction1.ts
+++ functions/src/FUNCTION_1/FUNC/runFunction1.ts
@@ -6,5 +6,5 @@ import * as functions from 'firebase-functions';
 exports = module.exports = functions.runWith({ timeoutSeconds: 540, memory: '256MB' })
     .https.onRequest((request, response) => {
         functions.logger.info("FUNCTION_1 Invoked!", {structuredData: true});
-        response.send("Hello from FUNCTION_1 Function!");
+        response.send("Hello from FUNCTION_1 Function! worked");
     });
