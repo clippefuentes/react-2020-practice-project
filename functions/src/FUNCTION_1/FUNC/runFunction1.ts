e12e45c76b16d69d2fe091f2bacd3b57e7f253b1
Clyne Paulo Fuentes <clippefuentes@gmail.com>
2020-11-25 10:32:13 -0700

    update yaml file nad edit function1

--- functions/src/FUNCTION_1/FUNC/runFunction1.ts
+++ functions/src/FUNCTION_1/FUNC/runFunction1.ts
@@ -6,5 +6,6 @@ import * as functions from 'firebase-functions';
 exports = module.exports = functions.runWith({ timeoutSeconds: 540, memory: '256MB' })
     .https.onRequest((request, response) => {
         functions.logger.info("FUNCTION_1 Invoked!", {structuredData: true});
+        functions.logger.info("Configs: ", functions.config(), {structuredData: true});
         response.send("Hello from FUNCTION_1 Function! worked");
     });
